e530e92707682acc2d8fdca63f97481ec0be10f9
Christopher <chrispritchard.nz@gmail.com>
2018-09-24 21:54:30 +1200

    Wrote make and move crack methods, as close to the source as possible

--- Program.fs
+++ Program.fs
@@ -1,9 +1,16 @@
 ﻿
 open Substrate
+open System.Drawing
+
+let width, height  = 900, 900
+let backColour = Color.White
 
 [<EntryPoint>]
-let main argv =
-    let out = match argv with [|outPath|] -> outPath | _ -> "./bin/result.bmp"
-    let bitmap = generate 900 900 defaultConfig
-    bitmap.Save out
+let main _ =
+    let bitmap = new Bitmap (width, height)
+    use graphics = Graphics.FromImage bitmap
+    let brush = new SolidBrush (backColour)
+    graphics.FillRectangle (brush, 0, 0, width, height)
+
+    bitmap.Save "./bin/result.bmp"
     0
--- Substrate.fs
+++ Substrate.fs
@@ -1,24 +1,60 @@
 module Substrate
-open System.Drawing
 
-type Config = {
-    minCracks: int
-    maxCracks: int
-    backColour: Color
-    crackColour: Color
-} 
-
-let defaultConfig = {
-    minCracks = 0
-    maxCracks = 200
-    backColour = Color.White
-    crackColour = Color.Black
+type World = {
+    grid: Map<int * int, int>
+    cracks: Crack list
+} and Crack = {
+    x: float
+    y: float
+    t: float
 }
 
-let generate (width: int) height config =
-    let bitmap = new Bitmap (width, height)
-    use graphics = Graphics.FromImage bitmap
-    let brush = new SolidBrush (config.backColour)
-    graphics.FillRectangle (brush, 0, 0, width, height)
+let private random = new System.Random (1)
+let private seeds = 16;
+let private cracks = 3
 
-    bitmap
+let crackJiggle () = (random.NextDouble() * 4.1) - 2.1
+let PI = System.Math.PI
+let z = 0.33
+let fuzz n = n + (random.NextDouble() * (z*2.)) - z |> int
+
+let makeCrack grid =
+    let index = random.Next <| Map.count grid
+    let (x, y), t = grid |> Map.toSeq |> Seq.item index
+
+    let T = 
+        if random.NextDouble() > 0.5 
+        then float t + 90. + crackJiggle ()
+        else float t - 90. + crackJiggle ()
+    let X = float x + 0.61*cos(T*PI/180.)
+    let Y = float y + 0.61*sin(T*PI/180.)
+
+    { x = X; y = Y; t = T % 360. }
+
+let moveCrack grid maxX maxY crack = 
+    let X = crack.x + 0.42*cos(t*PI/180.)
+    let Y = crack.y + 0.42*sin(t*PI/180.)
+
+    let x, y = fuzz X, fuzz Y
+    
+    if x < 0 || y < 0 || x >= maxX || y >= maxY then grid, None
+    else if grid |> Map.containsKey (x, y) |> not then Map.add (x, y) (int crack.t) grid, Some { crack with x = X; y = Y }
+    else
+        let current = float grid.[x, y]
+        if abs current - crack.t < 5. then Map.add (x, y) (int crack.t) grid, Some { crack with x = X; y = Y }
+        else if abs current - crack.t > 2. then grid, None
+        else grid, None
+
+let initWorld width height = 
+    let initialGrid = 
+        [1..seeds] 
+        |> List.map (fun _ -> 
+            (random.Next width, random.Next height), random.Next 360) 
+        |> Map.ofList
+    let initialCracks = 
+        [1..cracks] 
+        |> List.map (fun _ -> makeCrack initialGrid)
+    {
+        grid = initialGrid
+        cracks = initialCracks
+    }
