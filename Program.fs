d82ad0a6bde0b825ea9ce1a28bc631d4b842d7da
Christopher <chrispritchard.nz@gmail.com>
2018-09-25 18:47:12 +1200

    Implemented code to render grid to a bitmap

--- Program.fs
+++ Program.fs
@@ -4,6 +4,9 @@ open System.Drawing
 
 let width, height  = 900, 900
 let backColour = Color.White
+let foreColour = Color.Black
+
+let iterations = 1000
 
 [<EntryPoint>]
 let main _ =
@@ -11,6 +14,13 @@ let main _ =
     use graphics = Graphics.FromImage bitmap
     let brush = new SolidBrush (backColour)
     graphics.FillRectangle (brush, 0, 0, width, height)
+
+    let world = initWorld width height
+    let finalWorld = 
+        [1..iterations] |> Seq.fold (fun world _ -> advanceWorld world) world
+    
+    let brush = new SolidBrush (foreColour)
+    finalWorld.grid |> Map.iter (fun (x, y) _ -> graphics.FillRectangle(brush, x, y, 1, 1))
 
     bitmap.Save "./bin/result.bmp"
     0
